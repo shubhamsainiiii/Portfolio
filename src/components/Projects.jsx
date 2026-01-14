5dce1fb403364ec99ae3631051cf0e247bf93f70
Shubham Saini <shubhamthoi27@gmail.com>
2026-01-14 08:55:02 +0530

    portfolio link updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ const projects = [
             "Personal portfolio built with ReactJS showcasing projects, skills, and experience.",
         image: Hero,
         tech: ["React", "TailwindCSS"],
-        link: "https://shubhaminnovates.netlify.app/",
+        link: "www.shubhaminnovates.cloud",
         code: "https://github.com/shubhamsainiiii/Portfolio",
     },
     {
